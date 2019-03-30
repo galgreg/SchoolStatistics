df7ef6fd84ad70a1191abee6fbe0345d451b95a8
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-03-30 16:40:16 +0100

    Dołożenie testów klasy StatisticsGenerator do pliku test_main.cpp

--- school/test/test_main.cpp
+++ school/test/test_main.cpp
@@ -1,6 +1,7 @@
 #include "testidgenerator.h"
 #include "teststudent.h"
 #include "teststudentinfo.h"
+#include "teststatisticsgenerator.h"
 #include <QtTest/QtTest>
 
 int main(int argc, char** argv) {
@@ -14,6 +15,8 @@ int main(int argc, char** argv) {
     result |= QTest::qExec(&testStudent, argc, argv);
     TestStudentInfo testStudentInfo(passedCounter, failedCounter);
     result |= QTest::qExec(&testStudentInfo, argc, argv);
+    TestStatisticsGenerator testStatsGenerator(passedCounter, failedCounter);
+    result |= QTest::qExec(&testStatsGenerator, argc, argv);
 
     qDebug() << QString("Totals: %1 passed, %2 failed.")
                 .arg(passedCounter)
