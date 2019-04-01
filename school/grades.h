5010eab192044d13c02afbf696f64652081df512
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-04-01 11:26:31 +0200

    Uczynienie klasy Grades implementacją abstrakcyjnego interfejsu IGrades

--- school/grades.h
+++ school/grades.h
@@ -1,19 +1,20 @@
 #ifndef GRADES_H
 #define GRADES_H
 
+#include "igrades.h"
 #include <vector>
 
-class Grades
+class Grades : public IGrades
 {
 public:
     explicit Grades(size_t maximumCount = 3);
 
-    void add(double grade);
-    void remove(size_t whichGrade);
-    void edit(size_t whichGrade, double newValue);
-    size_t count() const;
-    double getGrade(unsigned whichGrade) const;
-    size_t maxAllowedCount() const;
+    void add(double grade) override;
+    void remove(size_t whichGrade) override;
+    void edit(size_t whichGrade, double newValue) override;
+    size_t count() const override;
+    double getGrade(unsigned whichGrade) const override;
+    size_t maxAllowedCount() const override;
 private:
     std::vector<double> grades;
     const size_t maxCount;
