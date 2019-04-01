0478d76bb490823fa31336829415ba87df6a1404
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-04-01 19:51:05 +0200

    Dodanie abstrakcyjnej klasy bazowej IStudentClass

---
+++ school/istudentclass.h
@@ -0,0 +1,20 @@
+#ifndef ISTUDENTCLASS_H
+#define ISTUDENTCLASS_H
+
+#include "istudent.h"
+#include <cstddef>
+
+class IStudentClass
+{
+public:
+    virtual ~IStudentClass() = default;
+    virtual size_t maxAllowedCount() const = 0;
+    virtual size_t count() const = 0;
+    virtual void addStudent(IStudent *newStudent) = 0;
+    virtual void removeStudent(size_t whichStudent) = 0;
+    virtual IStudent *getStudent(size_t whichStudent) = 0;
+    virtual void editStudent(size_t whichStudent, IStudent *newValue) = 0;
+    virtual void removeAll() = 0;
+};
+
+#endif // ISTUDENTCLASS_H
