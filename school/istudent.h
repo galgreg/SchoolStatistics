c7c9351c3d570f996921f9c5c25ee51711fa50d6
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-04-01 16:32:40 +0200

    Dodanie abstrakcyjnej klasy bazowej IStudent

---
+++ school/istudent.h
@@ -0,0 +1,16 @@
+#ifndef ISTUDENT_H
+#define ISTUDENT_H
+
+#include "ipersonaldata.h"
+#include "igrades.h"
+
+class IStudent {
+public:
+    virtual unsigned getID() const = 0;
+    virtual IPersonalData *getPersonalData() const = 0;
+    virtual IGrades *getGrades() const = 0;
+    virtual void setPersonalData(IPersonalData *newData) = 0;
+    virtual void setGrades(IGrades *newGrades) = 0;
+};
+
+#endif // ISTUDENT_H
