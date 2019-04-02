72bfb04e1a5bbd75a8d0f3e7a5fedd94ecc01897
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-04-02 11:58:23 +0200

    Praca nad testem jednostkowym TestTextFileStorage::testRead_OK()

--- school/test/testtextfilestorage.cpp
+++ school/test/testtextfilestorage.cpp
@@ -1,10 +1,26 @@
 #include "testtextfilestorage.h"
 #include "textfilestorage.h"
+#include "studentclass.h"
+#include "student.h"
+#include "personaldata.h"
+#include "grades.h"
 
 TestTextFileStorage::TestTextFileStorage(
         unsigned &passed,
         unsigned &failed) : TestExecutionCounter(passed, failed) {
 
+}
+
+void TestTextFileStorage::init() {
+    const QString filePath = QString::fromStdString(testFilePath);
+    QFile file(filePath);
+
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QTextStream outputStream(&file);
+        outputStream << "1 Jan Kowalski MALE 5.0 4.5 4.0" << "\n";
+        outputStream << "2 Maria Nowak FEMALE 4.0 3.5 3.0" << "\n";
+        file.close();
+    }
 }
 
 void TestTextFileStorage::cleanup() {
@@ -13,6 +29,7 @@ void TestTextFileStorage::cleanup() {
     } else {
         incrementPassCounter();
     }
+    remove(testFilePath.c_str());
 }
 
 void TestTextFileStorage::testDefaultState() {
@@ -29,4 +46,109 @@ void TestTextFileStorage::testRead_Error_NoSuchFile() {
     bool actualDoesFileExist = fileStorage.exist();
     QCOMPARE(actualDoesFileExist, expectedDoesFileExist);
     QVERIFY_EXCEPTION_THROWN(fileStorage.read(), std::ios_base::failure);
+}
+
+void TestTextFileStorage::testRead_OK() {
+    IStudent *expectedStudent_1 =
+            makeStudent(1, "Jan", "Kowalski", MALE, {5.0, 4.5, 4.0});
+    IStudent *expectedStudent_2 =
+            makeStudent(2, "Maria", "Nowak", FEMALE, {4.0, 3.5, 3.0});
+
+    const size_t maximumStudentsCount = 3;
+    IStudentClass *expectedStudentClass =
+            new StudentClass(maximumStudentsCount);
+
+    expectedStudentClass->addStudent(expectedStudent_1);
+    expectedStudentClass->addStudent(expectedStudent_2);
+
+    TextFileStorage fileStorage(testFilePath);
+    IStudentClass *actualStudentClass = fileStorage.read();
+
+    QVERIFY(actualStudentClass != nullptr);
+    compareClasses(actualStudentClass, expectedStudentClass);
+}
+
+IStudent* TestTextFileStorage::makeStudent(
+        unsigned ID,
+        const std::string &firstName,
+        const std::string &lastName,
+        Gender gender,
+        const std::vector<double> &grades) {
+    IStudent *student = new Student(ID);
+    IPersonalData *personalData =
+            new PersonalData(firstName, lastName, gender);
+    IGrades *studentGrades = new Grades(grades.size());
+
+    for(auto grade : grades) {
+        studentGrades->add(grade);
+    }
+
+    student->setPersonalData(personalData);
+    student->setGrades(studentGrades);
+    return student;
+}
+
+void TestTextFileStorage::compareClasses(
+        IStudentClass *actualClass,
+        IStudentClass *expectedClass) {
+    QVERIFY(actualClass != nullptr);
+    QVERIFY(expectedClass != nullptr);
+
+    const size_t actualClassCount = actualClass->count();
+    const size_t expectedClassCount = expectedClass->count();
+
+    QCOMPARE(actualClassCount, expectedClassCount);
+
+    for (size_t i = 0; i != actualClassCount; ++i) {
+        compareStudents(
+                actualClass->getStudent(i),
+                expectedClass->getStudent(i));
+    }
+}
+
+void TestTextFileStorage::compareStudents(
+        IStudent *actualStudent,
+        IStudent *expectedStudent) {
+    QVERIFY(actualStudent != nullptr);
+    QVERIFY(expectedStudent != nullptr);
+    QCOMPARE(actualStudent->getID(), expectedStudent->getID());
+
+    IPersonalData *actualPersonalData = actualStudent->getPersonalData();
+    IPersonalData *expectedPersonalData = expectedStudent->getPersonalData();
+    comparePersonalData(actualPersonalData, expectedPersonalData);
+
+    IGrades *actualGrades = actualStudent->getGrades();
+    IGrades *expectedGrades = expectedStudent->getGrades();
+    compareGrades(actualGrades, expectedGrades);
+}
+
+void TestTextFileStorage::comparePersonalData(
+        IPersonalData *actualPersonalData,
+        IPersonalData *expectedPersonalData) {
+    QVERIFY(actualPersonalData != nullptr);
+    QVERIFY(expectedPersonalData != nullptr);
+
+    QCOMPARE(
+            actualPersonalData->getFirstName(),
+            expectedPersonalData->getFirstName());
+    QCOMPARE(
+            actualPersonalData->getLastName(),
+            expectedPersonalData->getLastName());
+    QCOMPARE(
+            actualPersonalData->getGender(),
+                expectedPersonalData->getGender());
+}
+
+void TestTextFileStorage::compareGrades(
+        IGrades *actualGrades,
+        IGrades *expectedGrades) {
+    QVERIFY(actualGrades != nullptr);
+    QVERIFY(expectedGrades != nullptr);
+    QCOMPARE(actualGrades->count(), expectedGrades->count());
+
+    const size_t gradesCount = actualGrades->count();
+
+    for (unsigned i = 0; i != gradesCount; ++i) {
+        QCOMPARE(actualGrades->getGrade(i), expectedGrades->getGrade(i));
+    }
 }
--- school/test/testtextfilestorage.h
+++ school/test/testtextfilestorage.h
@@ -3,6 +3,7 @@
 
 #include "testexecutioncounter.h"
 #include <QtTest>
+#include "istudentclass.h"
 
 class TestTextFileStorage : public QObject, public TestExecutionCounter
 {
@@ -11,9 +12,28 @@ class TestTextFileStorage : public QObject, public TestExecutionCounter
 public:
     TestTextFileStorage(unsigned &passed, unsigned &failed);
 private slots:
+    void init();
     void cleanup();
     void testDefaultState();
     void testRead_Error_NoSuchFile();
+    void testRead_OK();
+private:
+    IStudent* makeStudent(
+        unsigned ID,
+        const std::string &firstName,
+        const std::string &lastName,
+        Gender gender,
+        const std::vector<double> &grades);
+    void compareClasses(
+        IStudentClass *actualClass,
+        IStudentClass *expectedClass);
+    void compareStudents(IStudent *actualStudent, IStudent *expectedStudent);
+    void comparePersonalData(
+        IPersonalData *actualPersonalData,
+        IPersonalData *expectedPersonalData);
+    void compareGrades(IGrades *actualGrades, IGrades *expectedGrades);
+
+    const std::string testFilePath = "testFile.txt";
 };
 
 #endif // TESTTEXTFILESTORAGE_H
