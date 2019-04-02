6c4b5985330dc74452233fbe39962b0bccd0991b
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-04-02 07:49:29 +0200

    Poprawa definicji interfejsowej klasy IDataRepository

--- school/idatarepository.h
+++ school/idatarepository.h
@@ -6,9 +6,16 @@
 class IDataRepository
 {
 public:
+    explicit IDataRepository(const std::string &repositoryPath);
     virtual ~IDataRepository() = default;
     virtual IStudentClass *read() = 0;
     virtual void write(IStudentClass *studentClass) = 0;
+
+    std::string getPath() {
+        return mRepositoryPath;
+    }
+private:
+    const std::string mRepositoryPath;
 };
 
 #endif // IDATAREPOSITORY_H
