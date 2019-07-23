73af400efaa0601d340f371c00c0768da8a0acb3
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-07-23 11:14:58 +0200

    Poprawa czytelności kodu w implementacji metod klasy ClassStatistics

--- school/classstatistics.cpp
+++ school/classstatistics.cpp
@@ -1,33 +1,34 @@
 #include "classstatistics.h"
 
 double ClassStatistics::getClassAverage(const IStudentClass& studentClass) {
+    double classAverage = 0.0;
     const size_t classCount = studentClass.count();
-    double gradesSum = 0.0;
 
-    if (classCount == 0) {
-        return gradesSum;
+    if (classCount > 0) {
+        double studentAveragesSum = 0.0;
+        for (size_t i = 0; i != classCount; ++i) {
+            studentAveragesSum += getStudentAverage(studentClass, i);
+        }
+        classAverage = studentAveragesSum / classCount;
     }
-
-    for (size_t i = 0; i != classCount; ++i) {
-        gradesSum += getStudentAverage(studentClass, i);
-    }
-    return static_cast<double>(gradesSum / classCount);
+    return classAverage;
 }
 
 double ClassStatistics::getStudentAverage(
         const IStudentClass& studentClass,
-        size_t whichStudent) {
+        size_t whichStudent) {    
     const IStudent& student = studentClass.getStudent(whichStudent);
     const IGrades& grades = student.getGrades();
-    double gradesSum = 0.0;
+
+    double studentAverage = 0.0;
     const size_t gradesCount = grades.count();
 
-    if (gradesCount == 0) {
-        return gradesSum;
-    }
-
-    for (unsigned i = 0; i != gradesCount; ++i) {
-        gradesSum += grades.getGrade(i);
-    }
-    return static_cast<double>(gradesSum / gradesCount);
+    if (gradesCount > 0) {
+        double gradesSum = 0.0;
+        for (unsigned i = 0; i != gradesCount; ++i) {
+            gradesSum += grades.getGrade(i);
+        }
+        studentAverage = gradesSum / gradesCount;
+    }    
+    return studentAverage;
 }
