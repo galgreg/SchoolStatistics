31c354310e580dcc44868f734c8992490069da2c
Grzegorz Galios <grzegorz.galios@wp.pl>
2019-07-27 12:57:51 +0200

    Przeniesienie pliku students.txt do głównego katalogu projektowego

--- school/main.cpp
+++ school/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QDir tempDir;
-    QString fileStorageDirLocation = tempDir.absolutePath() + "/../school/";
+    QString fileStorageDirLocation = tempDir.absolutePath() + "/../";
     const QString PATH_TO_STORAGE = fileStorageDirLocation + "students.txt";
     auto signalTransmitter = std::make_shared<SignalTransmitter>();
     MainWindow w(
